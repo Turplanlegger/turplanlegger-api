cbd25d08f9f8bdcdc2566b8395510491da856fdb
Are Schjetne <sixcare.as@gmail.com>
2022-04-17 13:23:20 +0200

    Init app

---
+++ turplanlegger/app.py
@@ -0,0 +1,36 @@
+from typing import Any, Dict
+
+from flask import Flask
+
+from turplanlegger.database.base import Database
+from turplanlegger.database.suds import Soap
+from turplanlegger.exceptions import ExceptionHandlers
+from turplanlegger.utils.config import Config
+from turplanlegger.utils.logger import Logger
+
+handlers = ExceptionHandlers()
+config = Config()
+logger = Logger()
+db = Database()
+soap = Soap()
+
+
+def create_app(config_override: Dict[str, Any] = None,
+               environment: str = None) -> Flask:
+
+    app = Flask(__name__)
+    app.config['ENVIRONMENT'] = environment
+    config.init_app(app)
+    app.config.update(config_override or {})
+
+    logger.setup_logging(app)
+
+    handlers.register(app)
+
+    db.init_db(app)
+    soap.init_client(app)
+
+    from turplanlegger.views import api
+    app.register_blueprint(api)
+
+    return app
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from turplanlegger import create_app
+
+app = create_app()
