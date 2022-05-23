9564bcdac548a946eef534e613f62f510e3ae7d5
Petter Sellesbakk Bjørkås <peseb@icloud.com>
2022-05-23 22:01:03 +0200

    small refactor

--- tests/test_route.py
+++ tests/test_route.py
@@ -14,18 +14,21 @@ class RoutesTestCase(unittest.TestCase):
         self.app = create_app(config)
         self.client = self.app.test_client()
 
-        # Users isn't implemented yet, so they have to be created manually for the time being
-
         self.user1 = {
             'name': 'Ola',
             'last_name': 'Nordamnn',
             'email': 'ola.nordmann@norge.no'
         }
-        self.user1 = {
+        self.user2 = {
             'name': 'Kari',
             'last_name': 'Nordamnn',
             'email': 'kari.nordmann@norge.no'
         }
+
+        db.create_user(self.user1["name"], self.user1["last_name"], self.user1["email"])
+        db.create_user(self.user2["name"], self.user2["last_name"], self.user2["email"])
+
+
         self.route = {
             'route': '{\"type\":\"LineString\",\"coordinates\":[[11.615295,60.603483],[11.638641,60.612921],[11.6819,60.613258],[11.697693,60.601797],[11.712112,60.586622],[11.703873,60.574476],[11.67984,60.568064],[11.640015,60.576838],[11.611862,60.587296]]}',
             'owner': 1,
@@ -38,7 +41,6 @@ class RoutesTestCase(unittest.TestCase):
         db.destroy()
 
     def test_create_route(self):
-        db.create_user(self.user1["name"], self.user1["last_name"], self.user1["email"])
         response = self.client.post('/route', data=json.dumps(self.route), headers=self.headers)
 
         self.assertEqual(response.status_code, 201)
@@ -47,13 +49,14 @@ class RoutesTestCase(unittest.TestCase):
         self.assertEqual(data['owner'], 1)
 
     def test_get_route(self):
-        db.create_user(self.user1["name"], self.user1["last_name"], self.user1["email"])
-        post_response = self.client.post('/route', data=json.dumps(self.route), headers=self.headers)
-        post_response_data = json.loads(post_response.data.decode('utf-8'))
-        created_route_id = post_response_data['id']
+        response = self.client.post('/route', data=json.dumps(self.route), headers=self.headers)
+        data = json.loads(response.data.decode('utf-8'))
+        created_route_id = data['id']
 
-        get_repsponse = self.client.get(f'/route/{created_route_id}')
-        self.assertEqual(get_repsponse.status_code, 200)
+        response = self.client.get(f'/route/{created_route_id}')
+        self.assertEqual(response.status_code, 200)
 
-        get_response_data = json.loads(get_repsponse.data.decode('utf-8'))
-        self.assertEqual(get_response_data['route']['owner'], post_response_data['owner'])
+        data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(data['route']['owner'], self.route['owner'])
+
+    # TODO: get_route(route_id), delete_route(route_id), add_route, change_route_owner(route_id)
