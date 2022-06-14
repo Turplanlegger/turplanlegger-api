807fdff11729887d6eb9d373e256a7af572ac88d
Petter <43450467+peseb@users.noreply.github.com>
2022-06-14 19:29:29 +0200

    Default config (#16)
    * multiline
    
    * precommit
    
    * cleanup variable

---
+++ turplanlegger/utils/default_config.py
@@ -0,0 +1,7 @@
+# Database
+DATABASE_URI = """postgresql://postgres:postgres@localhost/turplanlegger
+                ?connect_timeout=10&application_name=turplanlegger"""
+DATABASE_NAME = 'turplanlegger'
+
+# Logging
+LOG_TO_FILE = False
