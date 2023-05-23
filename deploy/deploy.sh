9eee3e1970a67ba11e3999d09bef2da2128a53cf
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-05-23 08:36:43 +0200

    Deploy script improvements (#109)
    * improve script
    
    * Remove extra newline from deploy script
    
    * Add --show-diff-on-failure to lint command
    
    * Remove trailing whitespace from deploy script
    
    * Add newline at end of file for deploy.sh
    
    * Minor improvements to curl
    
    * Removed the unnecessary use of .[] before select(). The select() function can directly operate on the array.
    * Updated the select() condition to use any() instead of iterating over the tags with contains(). It checks if any of the tags in the array is equal to "main".
    * Removed the unnecessary use of .metadata.container.tags[] inside select(). It is now .metadata.container.tags alone.
    
    ---------
    
    Co-authored-by: Are Schjetne <sixcare.as@gmail.com>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
-source $HOME/.config/turplanlegger/env
-NEW_SHA=$(curl -sH "Authorization: Bearer $GITHUB_ACCESS_TOKEN" https://api.github.com/user/packages/container/turplanlegger-api/versions | jq -r '.[] | select(.metadata.container.tags[] | contains("main")).name'
-)
-OLD_SHA=$(docker image inspect ghcr.io/sixcare/turplanlegger-api:main --format '{{json .RepoDigests}}' | jq .[0] | cut -d'@' -f2 | cut -d'"' -f1)
+set -e
 
-if [ $NEW_SHA != $OLD_SHA ]; then
-    cd $HOME/turplanlegger-api
-    curl -fsLO https://raw.githubusercontent.com/sixcare/turplanlegger-api/main/docker-compose.yml
-    docker compose pull
-    docker compose --env-file $HOME/.config/turplanlegger/env up -d
+# Source environment variables
+source "${HOME}/.config/turplanlegger/env"
+
+# Get the SHA of the new image
+NEW_SHA=$(curl -sH "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" "https://api.github.com/user/packages/container/turplanlegger-api/versions" | jq -r '.[] | select(.metadata.container.tags | any(. == "main")).name')
+if [[ -z "${NEW_SHA}" ]]; then
+    echo "Unable to retrieve the new image SHA"
+    exit 1
+fi
+
+# Get the SHA of the old image
+OLD_SHA=$(docker image inspect ghcr.io/sixcare/turplanlegger-api:main --format '{{.Id}}')
+if [[ -z "${OLD_SHA}" ]]; then
+    echo "Unable to retrieve the old image SHA"
+    exit 1
+fi
+
+# Compare SHAs and update the image if necessary
+if [[ "${NEW_SHA}" != "${OLD_SHA}" ]]; then
+    cd "${HOME}/turplanlegger-api"
+
+    # Backup current docker-compose file
+    cp docker-compose.yml docker-compose.yml.bak
+
+    # Download new docker-compose file
+    if ! curl -fsLO "https://raw.githubusercontent.com/sixcare/turplanlegger-api/main/docker-compose.yml"; then
+        echo "Failed to download docker-compose file, reverting to backup"
+        mv docker-compose.yml.bak docker-compose.yml
+        exit 1
+    fi
+
+    # Pull new images
+    if ! docker compose pull; then
+        echo "Failed to pull new Docker images"
+        exit 1
+    fi
+
+    # Restart services
+    docker compose --env-file "${HOME}/.config/turplanlegger/env" up -d
+else
+    echo "No new updates available"
 fi
