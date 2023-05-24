4b6e744ceb49f711c7f5b21fa99dcb8939f378eb
Are Schjetne <sixcare.as@gmail.com>
2023-05-24 23:27:08 +0200

    Improvements to deploy script

--- deploy/deploy-develop.sh
+++ deploy/deploy-develop.sh
@@ -2,31 +2,44 @@
 
 set -e
 
-source "${HOME}/.config/turplanlegger/env-develop"
+# Define variables
+ENV_FILE="${HOME}/.config/turplanlegger/env-develop"
+REPO_DIR="${HOME}/turplanlegger-api-develop/"
+COMPOSE_FILE="${REPO_DIR}/docker-compose.dev.yml"
 
-cd "${HOME}/turplanlegger-api-develop/"
-
-if ! git rev-parse --quiet --is-inside-work-treee > /dev/null 2>&1; then
-    printf "Not a git directory. Ensure it is\n"
+if ! git -C "$REPO_DIR" rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    printf "Not a git directory. Please ensure it is.\n"
     exit 1
 fi
 
-NEW_SHA=$(curl -sH "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" 'https://api.github.com/repos/sixcare/turplanlegger-api/commits?per_page=1&sha=develop' | jq -r .[0].sha)
-OLD_SHA=$(git log -n 1 --format=format:%H)
+if [[ -f "$ENV_FILE" ]]; then
+    source "$ENV_FILE"
+else
+    printf "Environment file not found: %s\n" "$ENV_FILE"
+    exit 1
+fi
 
-if [[ -z "${NEW_SHA}" ]]; then
+# Retrieve the latest commit SHA from the GitHub API
+NEW_SHA=$(curl -sH "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" "https://api.github.com/repos/sixcare/turplanlegger-api/commits?per_page=1&sha=develop" | jq -r '.[0].sha')
+
+OLD_SHA=$(git -C "$REPO_DIR" log -n 1 --format=format:%H)
+
+if [[ -z "$NEW_SHA" ]]; then
     printf "Unable to retrieve the new commit SHA\n"
     exit 1
 fi
 
-if [[ -z "${OLD_SHA}" ]]; then
+if [[ -z "$OLD_SHA" ]]; then
     printf "Unable to retrieve the old commit SHA\n"
     exit 1
 fi
 
-if [[ "${NEW_SHA}" != "${OLD_SHA}" ]]; then
-    git fetch --force --prune --prune-tags --quiet
-    git reset --quiet --hard origin/develop
-    docker compose --env-file "${HOME}/.config/turplanlegger/env-develop" -f docker-compose.dev.yml build --quiet
-    docker compose --env-file "${HOME}/.config/turplanlegger/env-develop" -f docker-compose.dev.yml up -d --quiet-pull --remove-orphans
+if [[ "$NEW_SHA" != "$OLD_SHA" ]]; then
+    # Fetch the latest changes from the remote develop branch
+    git -C "$REPO_DIR" fetch --force --prune --prune-tags --quiet
+    git -C "$REPO_DIR" reset --quiet --hard origin/develop
+
+    # Build and start Docker containers
+    docker-compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" build --quiet
+    docker-compose --env-file "$ENV_FILE" -f "$COMPOSE_FILE" up -d --quiet-pull --remove-orphans
 fi
