d92e3b51ad2d8594aaf14d2084f425bfbacc47aa
Are Schjetne <sixcare.as@gmail.com>
2023-09-25 14:39:12 +0200

    Added advisory message

--- turplanlegger/__about__.py
+++ turplanlegger/__about__.py
@@ -1,1 +1,3 @@
+# Editing this file on the main branch will trigger a GitHub Action that creates a new version tag.
+# After a new version tag is avaiable on GitHub, the new version will be build and published
 __version__ = '0.0.9b1'
