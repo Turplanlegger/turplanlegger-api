00aa4b820b6449667e96c1cbba015696ea7ec17b
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-01-16 20:00:09 +0100

    bump version number
    Testing the new release workflow.

--- turplanlegger/__about__.py
+++ turplanlegger/__about__.py
@@ -1,3 +1,3 @@
 # Editing this file on the main branch will trigger a GitHub Action that creates a new version tag.
 # After a new version tag is avaiable on GitHub, the new version will be built and published
-__version__ = '0.0.11b2'
+__version__ = '0.0.11b3'
