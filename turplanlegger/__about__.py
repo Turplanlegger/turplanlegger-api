6e426b754567d68703376bdf002defd03b00a224
Are Schjetne <sixcare.as@gmail.com>
2025-02-18 19:32:50 +0100

    🔖 Release 0.0.15b1
    This release contains only dependency updates.

--- turplanlegger/__about__.py
+++ turplanlegger/__about__.py
@@ -1,3 +1,3 @@
 # Editing this file on the main branch will trigger a GitHub Action that creates a new version tag.
 # After a new version tag is avaiable on GitHub, the new version will be built and published
-__version__ = '0.0.14b6'
+__version__ = '0.0.15b1'
