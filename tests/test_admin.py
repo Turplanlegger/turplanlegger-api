3e522d27d0ed046663214a74750dd50f98f74dac
Are Schjetne <sixcare.as@gmail.com>
2025-03-23 13:49:11 +0100

    🛂 Randomize the admin id

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -7,9 +7,9 @@ from turplanlegger.app import create_app, db
 class UsersTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        config = {'TESTING': True, 'SECRET_KEY': 'test', 'SECRET_KEY_ID': 'test', 'CREATE_ADMIN_USER': True}
+        cls.config = {'TESTING': True, 'SECRET_KEY': 'test', 'SECRET_KEY_ID': 'test', 'CREATE_ADMIN_USER': True, 'ADMIN_EMAIL': 'admin@admintestturplanlegger.com' , 'ADMIN_PASSWORD': 'tesme' }
 
-        cls.app = create_app(config)
+        cls.app = create_app(cls.config)
         cls.client = cls.app.test_client()
 
         response = cls.client.post(
@@ -30,13 +30,12 @@ class UsersTestCase(unittest.TestCase):
         db.destroy()
 
     def test_get_admin_user(self):
-        response = self.client.get('/users/06335e84-2872-4914-8c5d-3ed07d2a2f16', headers=self.headers)
+        response = self.client.get(f'/users?email={self.config.get("ADMIN_EMAIL")}', headers=self.headers)
         self.assertEqual(response.status_code, 200)
 
         data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(data['user']['id'], '06335e84-2872-4914-8c5d-3ed07d2a2f16')
         self.assertEqual(data['user']['name'], 'Admin')
         self.assertEqual(data['user']['last_name'], 'Nimda')
-        self.assertEqual(data['user']['email'], self.app.config.get('ADMIN_EMAIL'))
+        self.assertEqual(data['user']['email'], self.config.get('ADMIN_EMAIL'))
         self.assertEqual(data['user']['auth_method'], 'basic')
         self.assertEqual(data['user']['private'], True)
