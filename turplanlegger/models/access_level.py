4007c8181648899797d0a61c5803b9b8f9e9a202
Are Schjetne <sixcare.as@gmail.com>
2025-04-01 19:06:59 +0200

    ⚡️ Add comparison method for AccessLevel

--- turplanlegger/models/access_level.py
+++ turplanlegger/models/access_level.py
@@ -1,8 +1,27 @@
 
-from enum import Enum
+from enum import EnumMeta, StrEnum
 
 
-class AccessLevel(Enum):
-    READ = 'read'
-    MODIFY = 'modify'
-    DELETE = 'delete'
+class CachedEnumMeta(EnumMeta):
+    def __init__(cls, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Get index based on definition order
+        for index, member in enumerate(cls):
+            member._index = index
+
+class AccessLevel(StrEnum, metaclass=CachedEnumMeta):
+    READ = 'READ'
+    MODIFY = 'MODIFY'
+    DELETE = 'DELETE'
+
+    def __lt__(self, other):
+        return self._index < other._index
+
+    def __le__(self, other):
+        return self._index < other._index
+
+    def __gt__(self, other):
+        return self._index < other._index
+
+    def __ge__(self, other):
+        return self._index < other._index
